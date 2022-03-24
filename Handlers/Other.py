9cf648b4e6bdbca6739f524dfd403543ac7471b2
Semion_sh <vit@MacBook-Pro.local>
2022-03-24 21:51:03 +0300

    fds

--- Handlers/Other.py
+++ Handlers/Other.py
@@ -1,15 +1,16 @@
 from aiogram import types
 from create_bot import dp
+from Handlers.Admin import Name
 import json, string
 from aiogram.dispatcher import Dispatcher
 
 
-
 async def mat_block(message: types.Message):
-    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()} \
+    if {i.lower().replace(" ", "").translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()} \
             .intersection(set(json.load(open('cenz.json')))):
-        await message.reply('Ты же христианин, плохие слова запрещены!')
+        await message.reply(f'{Name}, ты же христианин, плохие слова запрещены!')
         await message.delete()
+
 
 def register_handlers_other(dp: Dispatcher):
     dp.register_message_handler(mat_block)
